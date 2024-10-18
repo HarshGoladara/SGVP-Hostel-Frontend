010116bb63579f38adfde3bd44fa0a4b2b83a29e
vasuparsaniya <vasuparsaniya21@gmail.com>
2024-10-18 22:00:53 +0530

    admission dev code merge

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import tseslintPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+          tsx: true,
+        },
       },
+      globals: {
+        ...globals.browser
+      }
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "@typescript-eslint": tseslintPlugin,
+      react: pluginReact,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // JavaScript rules
+      "eqeqeq": "off",
+      "no-unused-vars": "error", //warn
+      "max-len": ["warn", { code: 200 }],
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-use-before-define": "off",
+
+      // TypeScript rules
+      "@typescript-eslint/no-use-before-define": ["error"],
+
+      // React-specific rules
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
     },
   },
-]
+];
